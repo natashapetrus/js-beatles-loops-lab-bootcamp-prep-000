46ec504f5db844034544dfaca4886dfa9e90154a
Natasha Petrus <hello@natashapetrus.com>
2017-09-02 17:37:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-function theBeatlesPlay(){
+function theBeatlesPlay(musicians, instruments){
   var newArray = [];
+  for (let i=0; i<musicians.length; i++){
+    newArray = [...newArray, `${musicians[i]} plays ${instruments[i]}`];
+  }
   return newArray;
+}
+
+function johnLennonFacts(facts){
+  var newArray = [];
+  let i = 0;
+  while(i < facts.length){
+    newArray = [...newArray, `${facts[i]}!!!`];
+    i++;
+  }
+  return newArray;
+}
+
+function iLoveTheBeatles(){
+  
 }
